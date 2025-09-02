6739b0798bc3b823c6f6add1a94d5065610bcb9d
djh0w <44503877+Djh0w@users.noreply.github.com>
2025-09-02 03:04:55 +0000

    Add Initial page

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import { useState } from "react"
+
+function Home(){
+
+    const [like, setLike] = useState(0)
+
+    return (
+    <>
+        <h1>Desafio (Curso Dev)!</h1>
+        <button onClick={ () => {
+            setLike(like + 1)
+            alert("Obrigado pelo apoio!")
+        }            
+        }>Curtir</button>
+        
+        <h3>Total de amigos: <strong>{like}</strong></h3>
+    </>    
+    
+    )
+}
+
+export default Home
